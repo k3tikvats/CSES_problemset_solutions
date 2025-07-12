a1e1609ba08abd3dd6dbc717dc47216904e34584
k3tikvats <kartikvats_23cs214@dtu.ac.in>
2025-07-12 18:52:56 +0530

    tabulation for dice combinations

--- dp/1.cpp
+++ dp/1.cpp
@@ -45,13 +45,34 @@ void solvememo()
 	int n;
 	cin >> n;
 
-	int cnt = 0;
+	
 	vector<int> dp(n + 1, -1);
 
 	cout << helper(n, dp) << endl;
 	return;
 }
 
+void solve()
+{
+    // your code goes here
+
+    int n;
+    cin >> n;
+
+    
+    vector<int> dp(n + 1, -1);
+    dp[0] = 1; // Base case: there's one way to reach 0 (do nothing)
+    for (int i = 1; i <= n; i++){
+		dp[i]=0;
+        for(int j=1;j<=6;j++){
+            if(i-j>=0){
+				dp[i] = (dp[i] + dp[i - j]) % M; // Add ways from previous states
+			}
+        }
+    }
+    cout << dp[n] << endl;
+    return;
+}
 
 signed main()
 {
