19610ee49641cc6a29d274e98d897d5a6e3e9f0f
k3tikvats <kartikvats_23cs214@dtu.ac.in>
2026-05-25 19:49:48 +0530

    missingnumber and trailingzeroes added

---
+++ introductory/trailingzeroes.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define fl(i, n) for (int i = 0; i < n; i++)
+#define int long long
+#define ll long long
+#define in(n)    \
+    long long n; \
+    cin >> n;
+#define pb push_back
+#define eb emplace_back
+#define pp pop
+unsigned long long M = 1000000007;
+
+void solve()
+{
+    // your code goes here
+
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+
+    while (n)
+    {
+        n /= 5;
+        cnt += n;
+    }
+
+    cout << cnt << endl;
+}
+
+signed main()
+{
+    fastio int t = 1;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
